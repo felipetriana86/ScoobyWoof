2fda3a5be2846eb1e8f6e6a60e16880c571d0f53
felipetriana86 <gatosilvestre6910@gmail.com>
2023-02-27 19:33:27 -0500

    first commit

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
